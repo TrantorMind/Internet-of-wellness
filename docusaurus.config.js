a5a0dbfbcbd49f481a7b5a8a25c79cf771c8511a
GurYou <63901149+fcaroliAdmin@users.noreply.github.com>
2020-11-29 00:37:50 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,6 +6,7 @@ module.exports = {
   favicon: 'img/favicon.ico',
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
+  onBrokenLinks: 'ignore',
   themeConfig: {
     navbar: {
       title: 'My Site',
